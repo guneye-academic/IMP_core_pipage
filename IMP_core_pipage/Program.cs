e6842f2c21e3e96ab2dfc7aac5b6d0c3db6c9362
Evren <e.guney@akampus.com>
2020-01-22 22:42:42 +0300

    Add project files.

---
+++ IMP_core_pipage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMP_core_pipage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
